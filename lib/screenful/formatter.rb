e2e7fd91739ad95135ec996c8c0e8e2cdf941a9f
Piotrek Dubiel <piotr.dubiel@polidea.com>
2013-09-06 19:08:05 +0200

    Minimal dashboard

--- lib/screenful/formatter.rb
+++ lib/screenful/formatter.rb
@@ -1,86 +1,102 @@
-require 'calabash/formatters/html'
+require 'erb'
+require 'cucumber/formatter/io'
 
 module Screenful
-  class Formatter < Calabash::Formatters::Html
-    def initialize(step_mother, path, options)
-      super
-      @storyboard = ""
+  class Formatter
+    include Cucumber::Formatter::Io
+
+    def initialize(runtime, path, options)
+      @runtime = runtime
+      @htmldir = ensure_dir(path, "Screenful")
+      @img_id = 0
+      @id = ENV["DEVICE_ID"]
     end
 
     def before_features(features)
-      @step_count = features.step_count
-
-      @builder.head do
-        @builder.meta('http-equiv' => 'Content-Type', :content => 'text/html;charset=utf-8')
-        @builder.title 'Cucumber'
-        inline_css
-        inline_js
-      end
-      @builder << '<body>'
-      @builder << "<!-- Step count #{@step_count}-->"
-      @builder << '<div class="cucumber">'
-      @builder.div(:id => 'cucumber-header') do
-        @builder.div(:id => 'label') do
-          @builder.h1('Cucumber Features')
-        end
-        @builder.div(:id => 'summary') do
-          @builder.p('',:id => 'totals')
-          @builder.p('',:id => 'duration')
-          @builder.div(:id => 'expand-collapse') do
-            @builder.p('Expand All', :id => 'expander')
-            @builder.p('Collapse All', :id => 'collapser')
-          end
-        end
-      end
+      collect_subresults
+      copy_template
     end
 
     def after_features(features)
-      print_stats(features)
-      @builder << '</div>'
-      @builder << '</body>'
-    end
-
-    def embed_image(src, label)
-      if _output_relative? && _relative_uri?(src)
-        output_dir = Pathname.new(File.dirname(@io.path))
-        src_path = Pathname.new(src)
-        embed_relative_path = src_path.relative_path_from(output_dir)
-        src = embed_relative_path.to_s
-      end
-
-      id = "img_#{@img_id}"
-      @img_id += 1
-      if @storyboard != ""
-        @storyboard << image_separator 
-      end
-      @storyboard << %{
-        <span style="display: inline-block; margin-right: 10px;">
-          <img id="#{id}" style="height: 300px; display: block;" src="#{src}">
-          <h2 style="text-align: right;">#{@img_id}</h2>
-        </span>
-      }
-      @img_description = "<span>[see image #{@img_id}]</span>"
-    end
-
-    def after_step(step)
-      if @img_description
-        @builder << @img_description
-        @img_description = nil
-      end
-      super
-    end
-
-    def after_steps(steps)
-      super
-      @builder.div(:class => 'embed') do |pre|
-        pre << @storyboard
-      end
-      @storyboard = ""
+      @subresults << render_subresult
     end
 
     private
-    def image_separator
-      %{<span style="display: inline-block; margin-bottom: 140px; width:0; height:0; border-color: transparent transparent transparent #666; border-width: 20px; border-style: solid;"></span>}
+
+    def collect_subresults
+      @subresults = Dir.glob(@htmldir+'/result-*.html')
+    end
+
+    def copy_template
+      filename = File.dirname(__FILE__) + '/formatter/index.html.erb'
+      FileUtils.cp(filename, @htmldir)
+    end
+
+    def render_subresult
+      subresult_path = File.join(@htmldir, "result-#{@id}.html")
+      subresult = SubResult.new @id, @runtime
+      subresult.render(File.dirname(__FILE__) + '/formatter/result.html.erb', subresult_path)
+      FileUtils.cp(File.dirname(__FILE__) + '/formatter/bootstrap.css', @htmldir)
+      subresult_path
+    end
+
+    def render_result
+      result_path = File.join(@htmldir, "index.html")
+      summary = Summary.new(@subresults.map { |s| File.read(s) })
+      summary.render(File.dirname(__FILE__) + '/formatter/index.html.erb', result_path)
+      FileUtils.cp(File.dirname(__FILE__) + '/formatter/bootstrap.css', @htmldir)
+      result_path
+    end
+
+    class Result
+      def render(template, filename)
+        result = ERB.new(File.read(template)).result(binding)
+        File.open(filename, 'w') do |file|
+          file << result
+        end
+      end
+    end
+
+    class SubResult < Result
+      attr_reader :passed
+      attr_reader :skipped
+      attr_reader :failed
+      attr_reader :total
+
+      def initialize(id, runtime)
+        @device_name = "Device #{id}"
+        @total = runtime.scenarios.length
+
+        symbols = [:passed, :skipped, :failed]
+
+        metaclass = class << self; self; end
+        symbols.map do |status|
+          instance_variable_set("@#{status}", runtime.scenarios(status).length)
+          metaclass.send(:define_method, status.to_s + "_percentage") do
+            instance_variable_get("@#{status}").to_f * 100 / @total 
+          end
+        end
+
+        @scenarios = Hash[
+          *runtime.scenarios.map do |scenario|
+            status = case scenario.status
+                     when :passed then "success"
+                     when :failed then "error"
+                     when :skipped then "info"
+                     else ""
+                     end
+            [scenario.name, status]
+          end.flatten
+        ]
+      end
+    end
+
+    class Summary < Result
+      attr_reader :results
+
+      def initialize(results)
+        @results = results
+      end
     end
   end
 end
