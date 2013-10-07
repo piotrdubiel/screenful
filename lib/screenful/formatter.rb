84c5f121f300addaaf9d4189705cbe144cd255c9
Piotrek Dubiel <piotr.dubiel@polidea.com>
2013-10-07 16:06:54 +0200

    Images paths fix

--- lib/screenful/formatter.rb
+++ lib/screenful/formatter.rb
@@ -5,35 +5,9 @@ module Screenful
     def initialize(step_mother, path, options)
       super
       @storyboard = ""
+      @img_description = ""
     end
-
-    def before_features(features)
-      @step_count = features.step_count
-
-      @builder.head do
-        @builder.meta('http-equiv' => 'Content-Type', :content => 'text/html;charset=utf-8')
-        @builder.title 'Cucumber'
-        inline_css
-        inline_js
-      end
-      @builder << '<body>'
-      @builder << "<!-- Step count #{@step_count}-->"
-      @builder << '<div class="cucumber">'
-      @builder.div(:id => 'cucumber-header') do
-        @builder.div(:id => 'label') do
-          @builder.h1('Cucumber Features')
-        end
-        @builder.div(:id => 'summary') do
-          @builder.p('',:id => 'totals')
-          @builder.p('',:id => 'duration')
-          @builder.div(:id => 'expand-collapse') do
-            @builder.p('Expand All', :id => 'expander')
-            @builder.p('Collapse All', :id => 'collapser')
-          end
-        end
-      end
-    end
-
+   
     def after_features(features)
       print_stats(features)
       @builder << '</div>'
@@ -41,12 +15,10 @@ module Screenful
     end
 
     def embed_image(src, label)
-      if _output_relative? && _relative_uri?(src)
-        output_dir = Pathname.new(File.dirname(@io.path))
-        src_path = Pathname.new(src)
-        embed_relative_path = src_path.relative_path_from(output_dir)
-        src = embed_relative_path.to_s
-      end
+      output_dir = Pathname.new(File.dirname(@io.path))
+      src_path = Pathname.new(src)
+      embed_relative_path = src_path.relative_path_from(output_dir)
+      src = embed_relative_path.to_s
 
       id = "img_#{@img_id}"
       @img_id += 1
@@ -59,13 +31,13 @@ module Screenful
           <h2 style="text-align: right;">#{@img_id}</h2>
         </span>
       }
-      @img_description = "<span>[see image #{@img_id}]</span>"
+      @img_description << "<span>[see image #{@img_id}]</span>"
     end
 
     def after_step(step)
-      if @img_description
+      if @img_description && @img_description.empty?
         @builder << @img_description
-        @img_description = nil
+        @img_description = ""
       end
       super
     end
